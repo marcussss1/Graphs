c0331c098f98751a9f038f85b19663266e975681
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-04-29 14:15:11 +0300

    Add task description

--- task_2_2/task_2_2/main.cpp
+++ task_2_2/task_2_2/main.cpp
@@ -6,6 +6,22 @@
 //  Copyright © 2020 Mac-HOME. All rights reserved.
 //
 
+/*
+Дано число N < 106 и последовательность целых чисел из
+[-231..231] длиной N.
+
+Требуется построить бинарное дерево, заданное наивным
+порядком вставки.
+
+Т.е., при добавлении очередного числа K в дерево с корнем
+root, если root→Key ≤ K, то узел K добавляется в правое
+поддерево root; иначе в левое поддерево root.
+
+Требования: Рекурсия запрещена.
+
+Выведите элементы в порядке post-order (снизу вверх).
+*/
+
 #include <iostream>
 #include <functional>
 #include <stack>
@@ -43,8 +59,8 @@ class BST {
 public:
     BST();
     BST(const Compare& cmp);
-    BST(const BST& root) = delete;
-    BST(const BST&& root) = delete;
+    BST(const BST& tree) = delete;
+    BST& operator= (const BST& tree) = delete;
     ~BST();
     
     // bool has(const T& key) const;
